a3156ad80d20b46a4675baad10ad72df23df32bd
DJENNAOUI <djennaoui.raouf@gmail.com>
2023-10-25 12:47:01 +0100

    Initial commit

--- api_sm/models.py
+++ api_sm/models.py
@@ -9,8 +9,6 @@ class Interface_de_Connexion(models.Model):
     caption=models.CharField(max_length=100,null=True,blank=True)
     color=ColorField(null=True,blank=True,format="rgba",default="transparent")
     visible=models.BooleanField(default=True)
-
-
     def delete(self, *args, **kwargs):
         self.visible=False
         super(Interface_de_Connexion, self).save(*args, **kwargs)
