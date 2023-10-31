1aa802a9e0a13279e7d0f89d70ba81725d2fd616
DJENNAOUI <djennaoui.raouf@gmail.com>
2023-10-31 14:00:40 +0100

    Initial commit

--- api_sm/urls.py
+++ api_sm/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', login),
-    path('logout/', logout),
+    path('login/', LoginView.as_view()),
+    path('whoami/', WhoamiView.as_view()),
     path('ic_images/', GetICImages.as_view()),
     path('addclient/',AddClientView.as_view()),
 
--- api_sm/views.py
+++ api_sm/views.py
@@ -5,38 +5,32 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.views import APIView
+
 from .Serializers import *
 from .models import *
 
-@api_view(['POST'])
-@permission_classes([AllowAny])
-def login(request):
-    username = request.data.get('username')
-    password = request.data.get('password')
-    user = authenticate(username=username, password=password)
+class LoginView(APIView):
+    permission_classes = []
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
 
-    if user is not None:
-        token, created = Token.objects.get_or_create(user=user)
-        response=Response({'message': 'Invalid credentials'}, status=status.HTTP_200_OK)
-        response.set_cookie('token', token.key)
-        response.set_cookie('username', user.username)
-        response.set_cookie('id', user.id)
-        return response
-    else:
-        return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['POST'])
-@permission_classes([IsAuthenticated])
-def logout(request):
-
-    print("ij")
-    return Response({"error": "Invalid token"}, status=status.HTTP_400_BAD_REQUEST)
-
-# endpoint qui récupére le nom et id d'utilisateur
+        if user is not None:
+            token, created = Token.objects.get_or_create(user=user)
+            response=Response({'message': 'Invalid credentials'}, status=status.HTTP_200_OK)
+            response.set_cookie('token', token.key)
+            return response
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 
-
+class WhoamiView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        return Response({'whoami':request.user.username}, status=status.HTTP_200_OK)
 
 class GetICImages(generics.ListAPIView):
     permission_classes = []
@@ -45,10 +39,23 @@ class GetICImages(generics.ListAPIView):
 
 
 
-
 class AddClientView(generics.CreateAPIView):
-    queryset = Clients.objects.all()
-    serializer_class = AddClientsSerializer
+    permission_classes = [IsAuthenticated]
+    def post(self,request):
+        code_client=request.data.get('code_client')
+        type_client=request.data.get('type_client')
+        est_client_cosider=True
+        libelle_client=request.data.get('libelle_client')
+        nif=request.data.get('nif')
+        raison_social=request.data.get('raison_social')
+        num_registre_commerce=request.data.get('num_registre_commerce')
+        Clients(code_client=code_client,type_client=type_client
+                ,est_client_cosider=est_client_cosider,libelle_client=libelle_client
+                ,nif=nif,raison_social=raison_social,num_registre_commerce=num_registre_commerce).save()
+        return Response({'message':'123' }, status=status.HTTP_200_OK)
+
+
+
 
 
 
