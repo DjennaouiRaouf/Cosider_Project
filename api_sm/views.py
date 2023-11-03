63cf7dd6d5efff615836844f01963ff7699c388b
Djennaoui-Raouf <djennaoui.raouf@gmail.com>
2023-11-03 22:03:09 +0100

    Initial commit

--- api_sm/views.py
+++ api_sm/views.py
@@ -3,10 +3,8 @@ from rest_framework import generics
 from rest_framework.authtoken.models import Token
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
-
 from .Serializers import *
 from .models import *
 from .tools import *
