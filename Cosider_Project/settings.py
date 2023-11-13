481bf54aa0c22df9e2622074ab1ae9c65d1ee255
DJENNAOUI <djennaoui.raouf@gmail.com>
2023-11-13 12:51:57 +0100

    Initial commit

--- Cosider_Project/settings.py
+++ Cosider_Project/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'colorfield',
+    'model_history',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
