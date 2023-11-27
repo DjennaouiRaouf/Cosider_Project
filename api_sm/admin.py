c9c0dfb6d087b3c930b0e8bcceb3dcb474f55446
DJENNAOUI <djennaoui.raouf@gmail.com>
2023-11-27 13:49:33 +0100

    Initial commit

--- api_sm/admin.py
+++ api_sm/admin.py
@@ -142,10 +142,10 @@ class DQEAdmin(AdminFiltersMixin,SafeDeleteAdmin,SimpleHistoryAdmin,ImportExport
     save_as=True
     resource_class = DQEResource
     list_display = ("marche","designation","unite","quantite","prix_u","prix_q",)
-    list_filter = (SafeDeleteAdminFilter,
-                   ("marche__nt__code_site__code_site",ValueFilter.factory(lookup='exact')),
-                   ("marche__nt__nt", ValueFilter.factory(lookup='exact')),
-                   ("marche__num_avenant",NumberFilter ),
+    list_filter = ((SafeDeleteAdminFilter),
+                   ("marche__nt__code_site__code_site",ValueFilter.factory(lookup_name='exact')),
+                   ("marche__nt__nt", ValueFilter.factory(lookup_name='exact')),
+                   ("marche__num_avenant",ValueFilter.factory(lookup_name='exact') ),
                    )
     list_editable = ("prix_u",)
 
